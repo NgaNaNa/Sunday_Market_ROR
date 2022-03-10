c894af5051cb1ecaf2d54c68b60b7057d6db6dd8
Nga Rampling <ngarampling@Ngas-MacBook-Air.local>
2022-03-10 23:45:06 +1030

    adds rspec-rails rspec and updates routes

--- sunday_market/config/routes.rb
+++ sunday_market/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "listings#index"
+
+  get "/listings", to: "listings#index"
+  get "/users", to: "users#index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
