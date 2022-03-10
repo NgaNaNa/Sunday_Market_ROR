73d69645a30c24c3570ed3fc7d859f92ebeb46b4
Nga Rampling <ngarampling@Ngas-MacBook-Air.local>
2022-03-10 22:23:36 +1030

    recreate project with Postgresql as database

--- sunday_market/config/routes.rb
+++ sunday_market/config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  root "markets#index"
-
-  get "/markets", to: "markets#index"
-  get "/users", to: "users#index"
-  get "/listings", to: "listings#index"
-  
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
