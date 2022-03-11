43f9bf817ca938cbfb97899368879169472a7db2
Nga Rampling <ngarampling@Ngas-MacBook-Air.local>
2022-03-11 23:40:07 +1030

    creates users controller and updates routes

--- sunday_market/config/routes.rb
+++ sunday_market/config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root "listings#index"
 
   get "/listings", to: "listings#index", as: "listings"
-  get "/users", to: "users#index"
+  post "/listings", to: "listings#create"
+  get "/listings/:id", to: "listings#show", as: "listing"
+  get "/users", to: "users#index", as: "users"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
